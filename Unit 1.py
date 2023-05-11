80d4124e047f43a928c4d3c3feefc97ab86ba258
Trevinkydinkydon <123125715+Trevinkydinkydon@users.noreply.github.com>
2023-05-11 15:43:20 -0500

    Create Unit 1.py

---
+++ Unit 1.py
@@ -0,0 +1,23 @@
+print("Hello World!")
+
+"""
+This is a multi-line comment.
+It can span multiple lines.
+"""
+
+# This is a single-line comment.
+
+my_var = "selur nohtyp"
+
+print(my_var[0])  # Output: 's'
+
+print(my_var[-1])  # Output: 'p'
+
+print(my_var[::-1])  # Output: 'python rules'
+
+my_lst = ['first', 'second', 'third']
+
+print(my_lst[1])  # Output: 'second'
+
+my_lst.append('last')
+print(my_lst)  # Output: ['first', 'second', 'third', 'last']
